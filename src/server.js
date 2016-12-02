949b27671e35e50962951e5c4593084a7fe5ac6c
Daniel Kostro <kostro.d@gmail.com>
2016-12-02 12:53:15 +0100

    increase cache timeout

--- src/server.js
+++ src/server.js
@@ -8,10 +8,14 @@ const babelProxy = require('./babel-proxy');
 const proxy = require('http-proxy-middleware');
 const config = require('./config');
 
+const HOUR = 3600;
+const DAY = 24 * HOUR;
+const MONTH = 30 * DAY;
+
 app.use(cors());
 app.use(cacheControl({
-    browser: 3600,
-    server: 3600
+    browser: MONTH,
+    server: MONTH
 }));
 app.use(babelProxy());
 app.use(proxy({
