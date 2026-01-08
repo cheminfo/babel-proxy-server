54df848adb0c795f2b89994900d97b71351e53b2
Daniel Kostro <kostro.d@gmail.com>
2026-01-08 18:44:45 +0100

    refactor: remove docker requirement for local dev

--- src/addProxies.js
+++ src/addProxies.js
@@ -1,14 +1,11 @@
-import {existsSync, readFileSync} from "node:fs";
-import {join} from "node:path";
 import {createProxyMiddleware} from "http-proxy-middleware";
+import config from "./config.js";
 
 export default function addProxies(app) {
-  let proxies = [];
-
-  let proxyConfigFile = join(import.meta.dirname, "proxy.json");
-  if (existsSync(proxyConfigFile)) {
-    proxies = JSON.parse(readFileSync(proxyConfigFile, "utf8"));
-    for (let proxy of proxies) {
+  let additionalProxies = config.additionalProxies;
+  if (additionalProxies) {
+    for (let proxy of additionalProxies) {
+        console.log('Adding proxy', proxy.path, proxy.server);
       app.use(
         proxy.path,
         createProxyMiddleware({
